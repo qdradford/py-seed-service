118bbb1635c443d5c117b88ccf272322065a60b1
qrxcrk <qrxcrk@gmail.com>
2018-09-29 00:37:36 -0700

    update seed service and health check endpoint

--- src/modules/healthmodule.py
+++ src/modules/healthmodule.py
@@ -1,6 +1,9 @@
 #sytem imports
 import json
 
+import os
+from settings import APP_ROOT
+
 #helpers
 from helpers.configurations import configurations
 
@@ -8,11 +11,11 @@ class healthmodule():
     def __init__(self):
         self.configurations = configurations()
         self.diagnosticBaseUrl = self.configurations.getHealthUrl()
-        self.version_location = "../version.json"
+        self.version_location = "version.json"
         self.version_data = None
 
     def readVersion(self):
-        with open(self.version_location) as v:
+        with open(os.path.join(APP_ROOT, self.version_location)) as v:
             self.version_data = json.load(v)
         return self.version_data
 
---
+++ src/settings.py
@@ -0,0 +1,4 @@
+#settings.py
+import os
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
